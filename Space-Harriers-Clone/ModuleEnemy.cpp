15fc49aa959d958034e7d597e708a92cc6778050
Pere Viader Masuet <pere.viader22@gmail.com>
2017-11-19 17:53:06 +0100

    Created new module for instantiating enemies

---
+++ Space-Harriers-Clone/ModuleEnemy.cpp
@@ -0,0 +1,12 @@
+#include "ModuleEnemy.h"
+
+
+
+ModuleEnemy::ModuleEnemy()
+{
+}
+
+
+ModuleEnemy::~ModuleEnemy()
+{
+}
---
+++ Space-Harriers-Clone/ModuleEnemy.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "Module.h"
+
+class ModuleEnemy :
+	public Module
+{
+public:
+	ModuleEnemy();
+	~ModuleEnemy();
+};
+
