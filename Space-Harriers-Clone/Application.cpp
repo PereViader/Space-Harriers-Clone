f57936c9fc2e1b4a317596b5fd13fddacd36cfc0
Pere Viader Masuet <pere.viader22@gmail.com>
2017-11-22 12:47:38 +0100

    Changed module order. Collisions should be detected after particle movement

--- Space-Harriers-Clone/Application.cpp
+++ Space-Harriers-Clone/Application.cpp
@@ -40,8 +40,8 @@ Application::Application()
 
 
 	// Modules to draw on top of game logic
-	modules.push_back(collision = new ModuleCollision());
 	modules.push_back(particles = new ModuleParticles());
+	modules.push_back(collision = new ModuleCollision());
 
 	modules.push_back(fade = new ModuleFadeToBlack());
 }
