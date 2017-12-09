736ee72215e8338d031be0f29cebd156e6be3ee0
Pere Viader Masuet <pere.viader22@gmail.com>
2017-12-10 00:27:16 +0100

    Changed c cast to static_cast

--- Space-Harriers-Clone/RectUtil.h
+++ Space-Harriers-Clone/RectUtil.h
@@ -6,9 +6,9 @@
 
 inline SDL_Rect GetRectInPositionWithPivot(const Vector2& position, const Size2D& size, const Pivot2D& pivot) {
 	SDL_Rect rectForPivot;
-	rectForPivot.w = (int)(size.width);
-	rectForPivot.h = (int)(size.height);
-	rectForPivot.x = (int)(position.x - (size.width*pivot.x));
-	rectForPivot.y = (int)(position.y - (size.height*pivot.y));
+	rectForPivot.w = static_cast<int>(size.width);
+	rectForPivot.h = static_cast<int>(size.height);
+	rectForPivot.x = static_cast<int>(position.x - (size.width*pivot.x));
+	rectForPivot.y = static_cast<int>(position.y - (size.height*pivot.y));
 	return rectForPivot;
 }
