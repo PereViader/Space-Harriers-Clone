e4079fce8cbf569e5a4d030a9b337f8c81fa76c4
Pere Viader Masuet <pere.viader22@gmail.com>
2017-12-23 11:24:07 +0100

    Solved a bug where speed could not be negative. Now animation can play forward and backward. Added cpp file for animation

---
+++ Space-Harriers-Clone/Animation.cpp
@@ -0,0 +1,65 @@
+#include "Animation.h"
+
+Animation::Animation() :
+	loop(true),
+	speed(1.0f),
+	current_frame(0.0f),
+	loops(0)
+{}
+
+void Animation::UpdateFrame() {
+	/*float last_frame = (float)frames.size();
+
+	current_frame += speed*App->time->GetDeltaTime();
+	if (current_frame >= last_frame)
+	{
+	current_frame = (loop) ? 0.0f : max(last_frame - 1.0f, 0.0f);
+	loops++;
+	}*/
+
+	float maxFrameIndex = frames.size() - 1.0f;
+
+	assert(current_frame >= 0 && current_frame <= maxFrameIndex);
+
+	current_frame += speed * App->time->GetDeltaTime();
+
+	if (current_frame < 0 || current_frame > maxFrameIndex) {
+		if (loop) {
+			float newFrame = fmod(current_frame, maxFrameIndex);
+			if (newFrame != current_frame) loops++;
+			current_frame = newFrame;
+			assert(current_frame >= 0 && current_frame <= maxFrameIndex);
+
+		}
+		else {
+			current_frame = min(maxFrameIndex, max(0.0f, current_frame));
+			assert(current_frame >= 0 && current_frame <= maxFrameIndex);
+
+		}
+	}
+
+	assert(current_frame >= 0 && current_frame <= maxFrameIndex);
+
+}
+
+const SDL_Rect& Animation::GetFrame() const
+{
+	return frames[(int)current_frame];
+}
+
+const SDL_Rect& Animation::UpdateAndGetFrame()
+{
+	UpdateFrame();
+	return GetFrame();
+}
+
+bool Animation::Finished() const
+{
+	return loops > 0;
+}
+
+void Animation::Reset()
+{
+	current_frame = 0.0f;
+	loops = 0;
+}
--- Space-Harriers-Clone/Animation.h
+++ Space-Harriers-Clone/Animation.h
@@ -1,6 +1,10 @@
-#pragma once
+#ifndef _ANIMATION_H_
+#define _ANIMATION_H_
+
 #include <vector>
 #include <algorithm>
+#include <assert.h>
+
 #include "Application.h"
 #include "ModuleTime.h"
 
@@ -9,6 +13,16 @@ using namespace std;
 class Animation
 {
 public:
+	Animation();
+
+	void UpdateFrame();
+	const SDL_Rect& GetFrame() const;
+	const SDL_Rect& UpdateAndGetFrame();
+
+	bool Finished() const;
+	void Reset();
+
+public:
 	bool loop;
 	float speed;
 	vector<SDL_Rect> frames;
@@ -16,47 +30,6 @@ public:
 private:
 	float current_frame;
 	int loops;
-
-public:
-	Animation() : 
-		loop(true),
-		speed(1.0f),
-		current_frame(0.0f),
-		loops(0)
-	{}
-
-	Animation(const Animation& anim) : loop(anim.loop), speed(anim.speed), frames(anim.frames)
-	{}
-
-	void UpdateFrame() {
-		float last_frame = (float)frames.size();
-
-		current_frame += speed*App->time->GetDeltaTime();
-		if (current_frame >= last_frame)
-		{
-			current_frame = (loop) ? 0.0f : max(last_frame - 1.0f, 0.0f);
-			loops++;
-		}
-	}
-
-	const SDL_Rect& GetFrame() const
-	{
-		return frames[(int)current_frame];
-	}
-
-	const SDL_Rect& UpdateAndGetFrame()
-	{
-		UpdateFrame();
-		return GetFrame();
-	}
-
-	bool Finished() const
-	{
-		return loops > 0;
-	}
-
-	void Reset()
-	{
-		current_frame = 0.0f;
-	}
 };
+
+#endif // !_ANIMATION_H_
