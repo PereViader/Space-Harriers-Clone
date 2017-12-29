1b6f9a741518831d3094973b89cd11fe25306233
Pere Viader Masuet <pere.viader22@gmail.com>
2017-12-29 11:34:27 +0100

    Implemented explosion behaviour

--- Space-Harriers-Clone/Explosion.cpp
+++ Space-Harriers-Clone/Explosion.cpp
@@ -1,11 +1,15 @@
 #include "Explosion.h"
 
 #include "FloorAnchoredTransform.h"
+#include "Application.h"
+#include "ModuleRender.h"
+#include "Pivot2D.h"
 
-Explosion::Explosion(const Texture& graphics, const Animation& animation) :
+Explosion::Explosion(const Texture& graphics, const Animation& animation, float scalingFactor) :
 	Enemy(new FloorAnchoredTransform(), false),
 	graphics(graphics),
-	animation(animation)
+	animation(animation),
+	scalingFactor(scalingFactor)
 {
 }
 
@@ -16,17 +20,40 @@ Explosion::~Explosion()
 
 void Explosion::OnCollision(const Collider & own, const Collider & other)
 {
+	// has no collision
 }
 
 void Explosion::Init(map<string, void*> values)
 {
+	// nothing to init
 }
 
 void Explosion::Update()
 {
+	if (GetTransform().GetDepth() <= 0 || animation.Finished()) {
+		OnExplosionDied();
+	}
 }
 
 Explosion * Explosion::Clone() const
 {
 	return new Explosion(*this);
+}
+
+void Explosion::Render()
+{
+	Enemy::Render();
+
+	if (!ToDelete()) {
+		Vector3 screen = GetTransform().GetScreenPositionAndDepth();
+		float scale = scalingFactor * GetTransform().GetRenderingScale();
+		animation.UpdateFrame();
+		graphics.UpdateTexture(animation);
+		App->renderer->BlitWithPivotScaledZBuffer(graphics, scale, Pivot2D::BOTTOM_CENTER, screen);
+	}
+}
+
+void Explosion::OnExplosionDied()
+{
+	MarkAsDeleted();
 }
--- Space-Harriers-Clone/Explosion.h
+++ Space-Harriers-Clone/Explosion.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "Enemy.h"
 
+#include "FloorAnchoredTransform.h"
+
 #include "Texture.h"
 #include "Animation.h"
 
@@ -8,16 +10,22 @@ class Explosion :
 	public Enemy
 {
 public:
-	Explosion(const Texture& graphics, const Animation& animation);
+	Explosion(const Texture& graphics, const Animation& animation, float scalingFactor);
 	virtual ~Explosion();
 
 	virtual void OnCollision(const Collider & own, const Collider & other) override;
 	virtual void Init(map<string, void*> values) override;
 	virtual void Update() override;
 	virtual Explosion * Clone() const override;
+	virtual void Render() override;
+	virtual FloorAnchoredTransform& GetTransform() { return GetTransformTypped<FloorAnchoredTransform>(); }
 
 private:
 	Texture graphics;
 	Animation animation;
+	float scalingFactor;
+
+private:
+	void OnExplosionDied();
 };
 
