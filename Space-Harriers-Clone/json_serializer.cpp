aa22d85936e3c75237026d3b5d55ff08101e7ea1
Pere Viader Masuet <pere.viader22@gmail.com>
2017-12-30 12:30:29 +0100

    Created cpp file for json serializer and moved definition of the functions to it

---
+++ Space-Harriers-Clone/json_serializer.cpp
@@ -0,0 +1,62 @@
+#include "json_serializer.h"
+
+#include <vector>
+
+#include "SDL\include\SDL.h"
+#include "json.hpp"
+
+#include "Vector3.h"
+#include "Vector2.h"
+#include "Size2D.h"
+#include "Animation.h"
+
+using nlohmann::json;
+
+void to_json(json & j, const Vector3 & v) {
+	j = json{ { "x",v.x },{ "y",v.y },{ "z",v.z } };
+}
+
+void from_json(const json & j, Vector3 & v) {
+	v.x = j.at("x").get<float>();
+	v.y = j.at("y").get<float>();
+	v.z = j.at("z").get<float>();
+}
+
+void to_json(json & j, const Vector2 & v) {
+	j = json{ { "x",v.x },{ "y",v.y } };
+}
+
+void from_json(const json & j, Vector2 & v) {
+	v.x = j.at("x").get<float>();
+	v.y = j.at("y").get<float>();
+}
+
+void to_json(json & j, const Size2D & s) {
+	j = json{ { "width", s.width },{ "height",s.height } };
+}
+
+void from_json(const json & j, Size2D & v) {
+	v.width = j.at("width").get<float>();
+	v.height = j.at("height").get<float>();
+}
+
+void to_json(json & j, const Animation & a) {
+	j = json{ { "frames", a.frames },{ "loop", a.loop },{ "speed",a.speed } };
+}
+
+void from_json(const json & j, Animation & a) {
+	a.frames = j.at("frames").get<vector<SDL_Rect>>();
+	a.loop = j.at("loop").get<bool>();
+	a.speed = j.at("speed").get<float>();
+}
+
+void to_json(json & j, const SDL_Rect & a) {
+	j = json{ a.x,a.y,a.w,a.h };
+}
+
+void from_json(const json & j, SDL_Rect & a) {
+	a.x = j.at(0).get<int>();
+	a.y = j.at(1).get<int>();
+	a.w = j.at(2).get<int>();
+	a.h = j.at(3).get<int>();
+}
--- Space-Harriers-Clone/json_serializer.h
+++ Space-Harriers-Clone/json_serializer.h
@@ -1,63 +1,27 @@
-#pragma once
+#ifndef _JSON_SERIALIZER_H_
+#define _JSON_SERIALIZER_H_
 
 #include "json.hpp"
-
-#include "Vector3.h"
-#include "Vector2.h"
-#include "Size2D.h"
-
-#include <vector>
-#include "SDL\include\SDL.h"
-
-using namespace std;
-
 using nlohmann::json;
 
-void to_json(json& j, const Vector3& v) {
-	j = json{ {"x",v.x}, {"y",v.y}, {"z",v.z} };
-}
+class Vector3;
+void to_json(json& j, const Vector3& v);
+void from_json(const json& j, Vector3& v);
 
-void from_json(const json& j, Vector3& v) {
-	v.x = j.at("x").get<float>();
-	v.y = j.at("y").get<float>();
-	v.z = j.at("z").get<float>();
-}
+class Vector2;
+void to_json(json& j, const Vector2& v);
+void from_json(const json& j, Vector2& v);
 
-void to_json(json& j, const Vector2& v) {
-	j = json{ { "x",v.x },{ "y",v.y } };
-}
+class Size2D;
+void to_json(json& j, const Size2D& s);
+void from_json(const json& j, Size2D& v);
 
-void from_json(const json& j, Vector2& v) {
-	v.x = j.at("x").get<float>();
-	v.y = j.at("y").get<float>();
-}
+class Animation;
+void to_json(json& j, const Animation& a);
+void from_json(const json& j, Animation& a);
 
-void to_json(json& j, const Size2D& s) {
-	j = json{ { "width", s.width },{ "height",s.height } };
-}
+struct SDL_Rect;
+void to_json(json& j, const SDL_Rect& a);
+void from_json(const json& j, SDL_Rect& a);
 
-void from_json(const json& j, Size2D& v) {
-	v.width = j.at("width").get<float>();
-	v.height = j.at("height").get<float>();
-}
-
-void to_json(json& j, const Animation& a) {
-	j = json{ {"frames", a.frames}, {"loop", a.loop}, {"speed",a.speed} };
-}
-
-void from_json(const json& j, Animation& a) {
-	a.frames = j.at("frames").get<vector<SDL_Rect>>();
-	a.loop = j.at("loop").get<bool>();
-	a.speed = j.at("speed").get<float>();
-}
-
-void to_json(json& j, const SDL_Rect& a) {
-	j = json{a.x,a.y,a.w,a.h};
-}
-
-void from_json(const json& j, SDL_Rect& a) {
-	a.x = j.at(0).get<int>();
-	a.y = j.at(1).get<int>();
-	a.w = j.at(2).get<int>();
-	a.h = j.at(3).get<int>();
-}
+#endif // !_JSON_SERIALIZER_H_
